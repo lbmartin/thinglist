1d5851421cfe3aea9a52f9ceb80b1e23f91d86f9
Leo Martin <lbmartin@butler.edu>
2017-05-24 09:38:17 -0400

    Sort things by Id

--- src/ThingList.js
+++ src/ThingList.js
@@ -3,11 +3,15 @@ import './ThingList.css'
 import Thing from './Thing'
 
 const ThingList = (props) => {
+    const sortThings = (a,b) => {
+        return b.match(/\d+/)[0] - a.match(/\d+/)[0]
+    }
     return(
         <ul className="ThingList">
             { 
                 Object
                   .keys(props.things)
+                  .sort(sortThings)
                   .map(thingId => <Thing key={thingId} thing={props.things[thingId]} onChange={props.onChange} />) 
             }
         </ul>
