f8113cccdc055fccf92590279adb25703777a48e
Leo Martin <lbmartin@butler.edu>
2017-05-25 10:26:00 -0400

    Add github authentication

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
+import { auth, githubProvider } from './base'
 import './SignIn.css'
 
 const SignIn = () => {
+  const authenticate = (provider) => {
+      auth.signInWithPopup(githubProvider)
+  }
+
     return(
-        <button className="Login">
+        <button 
+          className="Login" 
+          onClick={() => authenticate(githubProvider)}
+          >
           Sign In With GitHub
         </button>
     )
